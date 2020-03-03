b028929603750cdd8edb1ce0eb4a25dab2c98125
Andrew Philpott and Krista Rutz <andrew+krista@>
2020-03-03 13:39:03 -0800

    Create user interface and enum class with allergies to produce list of allergies based on number entered.

--- Allergies/Models/Allergy.cs
+++ Allergies/Models/Allergy.cs
@@ -5,5 +5,18 @@ namespace Allergies.Models
   public class Allergy
   {
 
+    [Flags]
+    public enum AllergiesList
+    {
+      None = 0,
+      Eggs = 1,
+      Peanuts = 2,
+      Shellfish = 4,
+      Strawberries = 8,
+      Tomatoes = 16,
+      Chocolate = 32,
+      Pollen = 64,
+      Cats = 128
+    }
   }
 }
--- Allergies/Program.cs
+++ Allergies/Program.cs
@@ -7,7 +7,24 @@ namespace Allergies
   {
     public static void Main()
     {
-
+      Console.WriteLine("List the allergies you have by adding up their scores");
+      for (int i = 1; i <= 128; i *= 2)
+      {
+        Console.WriteLine("Score: " + i + " \t" + (Allergy.AllergiesList)i);
+      }
+      string scoreString = Console.ReadLine();
+      int score;
+      bool success = int.TryParse(scoreString, out score);
+      if (success)
+      {
+        var allergyResults = (Allergy.AllergiesList)score;
+        Console.WriteLine(allergyResults);
+        Console.ReadLine();
+      }
+      else
+      {
+        Main();
+      }
     }
   }
 }
